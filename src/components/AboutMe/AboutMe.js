74695949ca110fac03a2a197b7d7e5ebcd8a3112
BabulKumar2022 <bb3583400@gmail.com>
2022-04-18 19:42:17 +0600

    blogs page added

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,13 +4,19 @@ import './AboutMe.css'
 const AboutMe = () => {
     return (
         <div className='about-container'>
-            <h1 className='about-header text-center'>About me</h1>
-            <div className="about-img">
-            <img src="./" alt="" />
+            <div className="row col-4  mx-auto">
+                <h1 className='about-header text-center'>About me</h1>
+                <div className="about-img ">
+                     <img src="./images/tutor (2).jpg" alt="" />
+                </div>
+                <div className="about-description mt-3 ">
+                    <h4>Name: Babul Kumar Biswas </h4> 
+                    <h5> Bss(Hons), MSS in Economics</h5>
+                    <p>I am a professional private tutor. I help all students about Math,Science, Computer and more....</p>
+
+                </div>
             </div>
-            <div className="about-description">
-                <h4>Name: </h4> 
-            </div>
+            
            
 
         </div>
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
         <div>
-            <h3>Blogs</h3>
+           
+            <div className="container">
+                <div className="row col-6 mx-auto">
+                <h3 className='text-center'>Blogs Page</h3>
+                    <h4>প্রশ্নঃ authorization এবং authentication এর মধ্যে পার্থক্য কি?</h4> 
+                    <p>authorization হল প্রমানিকরন, অর্থাৎ ওয়েবের ক্ষেত্রে ইউজারের আইডি, পাসওয়ার্ড, নাম, ইমেইল  সংরক্ষণ করে এবং  পরে লগইন করের সময় চেক করে মেলে গেলে লগইন এক্সেস দেয় নতুবা দেয় না। অন্য দিকে authentication হল অনুমোদন দেওয়া, ফাইল,ডাটা, ইমেইল অন্য প্রতিষ্ঠানকে শর্ত সাপেক্ষে অনুমোদন দেওয়া।</p>  
+                    <br />
+                    <h5>প্রশ্নঃ Firebase আমরা কেন ব্যবহার করি?</h5>
+                    <p>ফায়রারবেজ হল গুগলের একটি ওয়েব ডেভলেপমেন্ট সারভিস। ওয়েব এয়্যাপ তৈরির জন্য এটি ব্যাবহার করা হয়।Web, Android, iOS এর ব্য্যাকেন্ড  ্ডেভলপমেন্ট এর কাজে ফায়ারবেজ ব্যবহার করা হয়। Firebase এর  Database, Cloud Storage, Cloud Messaging,Remote Config সার্ভিস আমরা ব্যবহার করে অনেক কম সময়ে অনেক কাজ করতে পারি। </p>
+
+
+                </div>
+            </div>
         </div>
     );
 };
